d77690e6206910ba0d32cb5637f38beb5e9a09dd
Matheus Heinzen <168302386+MatheusHeinzen@users.noreply.github.com>
2024-10-30 18:43:21 -0300

    Comentarios 1.1
    Tinha esquecido desse.

--- Pintura.java
+++ Pintura.java
@@ -1,13 +1,16 @@
 public class Pintura extends Arte {
+    //Atributos
     private String traco;
     private String dimensao;
 
+    //Construtor
     public Pintura(String nome, String autor, String descricao, int anoPublicacao, String dimensao, double valorArte) {
         super(nome, autor, descricao, anoPublicacao, valorArte);
         this.traco = traco;
         this.dimensao = dimensao;
     }
 
+    //Métodos
     public void listarObras(){
 
     }
